f9c52bfb5e4e6d9a99c0e5b2dab1fd5ce3192c3d
test <test@qq.com>
2022-08-30 16:48:03 +0800

    1

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2022/8/30
+ * Time: 16:38
+ */
+phpinfo();
--- js/logout.js
+++
@@ -1,9 +0,0 @@
-function activate_logout_js(){
-    
-    // add log out controller to log_out button
-    $('#log-out').click(function(){
-        $('.app').css('display', 'none');
-        $('.login-window').css('display', 'flex'); 
-    })
- 
-}
